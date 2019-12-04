aef11e975dcabf9336289f2a24caf641fafc4926
Tony Zhu <snickerton@users.noreply.github.com>
2019-12-03 19:22:51 -0600

    Add occurences counting, if drawer occurences is 0, if chosen it is 1

--- mousepos.py
+++ mousepos.py
@@ -1,14 +1,22 @@
 import pyautogui
 import pyperclip
 import time
+import pickle
 
 
-#
 # text = open("mainscreen.txt").read().split()
 # print(text)
 # paste = pyperclip.paste().split()
 # print(paste)
-# print(text == paste)
-while(True):
-    print(pyautogui.position())
-    time.sleep(.5)
+# print(text[0:14] == paste[0:14])
+# while(True):
+#     print(pyautogui.position())
+#     time.sleep(.5)
+d = {}
+with open('wordlist.pickle', 'rb') as handle:
+    d = pickle.load(handle)
+for x in d.keys():
+    d[x] = 0
+print(d)
+with open('wordlistFinal.pickle', 'wb') as handle:
+    pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
--- wordlist.py
+++ wordlist.py
@@ -17,14 +17,17 @@ def selectAllPaste():
     pyautogui.hotkey('ctrl', 'a')
     pyautogui.hotkey('ctrl', 'c')
     return pyperclip.paste()
+
+
+
 toplist, winlist = [], []
 def enum_cb(hwnd, results):
     winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
 win32gui.EnumWindows(enum_cb, toplist)
 
 wordlist = {}
-
-with open('wordlist2.pickle', 'rb') as handle:
+filename = 'wordlistFinal.pickle'
+with open(filename, 'rb') as handle:
     wordlist = pickle.load(handle)
 
 # readable = open("wordlist.txt", "w")
@@ -59,7 +62,8 @@ win32gui.SetForegroundWindow(hwin)
 bbox = win32gui.GetWindowRect(hwin)
 
 lastWordSaved = "N/A"
-
+# words that have already been registered, each session is only like 20 min max so we don't have to worry about large data sizes for this
+caughtWords = []
 while(True):
     print(datetime.datetime.now())
     print("\t\t LAST WORD SAVED: ", lastWordSaved)
@@ -71,18 +75,29 @@ while(True):
     time.sleep(.1)
 
     text = pyperclip.paste()
-    if text.split() == open("disconnectscreen.txt").read().split():
+
+    if text.split() == open("mainscreen.txt").read().split():
         # main menu screen
-        print("connection lost detected")
-        pyautogui.click(x=2871, y=600, clicks=1, button='left')
-        time.sleep(.5)
-        pyautogui.click(x=2871, y=600, clicks=1, button='left')
+        print("mainscreen detected")
+        caughtWords = []
         time.sleep(.5)
         pyautogui.click(x=2652, y=539, clicks=1, button='left')
         time.sleep(5)
-    elif text.split() == open("mainscreen.txt").read().split():
+        # arbitrary amount of first couple of words just to confirm it's a variation of the main menu screen
+    elif text.split()[0:14] == open("mainscreen.txt").read().split()[0:14]:
         # main menu screen
-        print("mainscreen detected")
+        # x=2652, y=539 original ok button
+        print("boot detected")
+        # reset words that you've already seen
+        caughtWords = []
+        # click out the notification window a couple of times
+        pyautogui.click(x=2294, y=625, clicks=1, button='left')
+        time.sleep(.1)
+        pyautogui.click(x=2294, y=625, clicks=1, button='left')
+        time.sleep(.1)
+        pyautogui.click(x=2294, y=625, clicks=1, button='left')
+        time.sleep(.3)
+        pyautogui.click(x=2294, y=625, clicks=1, button='left')
         time.sleep(.5)
         pyautogui.click(x=2652, y=539, clicks=1, button='left')
         time.sleep(5)
@@ -93,22 +108,29 @@ while(True):
             if x.startswith("Choose a word"):
                 for bob in lines[i+1:i+4]:
                     if bob not in wordlist:
-                        print("NEW WORD FOUND")
-                        print(bob)
-                        lastWordSaved = datetime.datetime.now()
+                        lastWordSaved = "New word found: " + bob + " | " + str(datetime.datetime.now())
+                        print(lastWordSaved)
                         wordlist[bob] = 0
+                        # wordlist[bob] = len(bob)
             if x.startswith("The word was '"):
                 word = x[14:len(x)-1]
-                if word not in wordlist:
-                    print("NEW WORD FOUND")
-                    print(word)
-                    lastWordSaved = datetime.datetime.now()
-                    wordlist[word] = 1
-                else:
-                    print("Logging occurence: ", word)
-                    wordlist[word] = wordlist[word]+1
+                if word not in caughtWords:
+                    lastWordSaved =  word + " | " + str(datetime.datetime.now())
+                    print("New word found: " + lastWordSaved)
+                    wordlist[word] = wordlist.get(word, 0) + 1
+                    print("\tPrev Count: ",wordlist[word]-1, " | New Count: ", wordlist[word])
+                    caughtWords.append(word)
+                # if word not in wordlist:
+                #     print("NEW WORD FOUND")
+                #     print(word)
+                #     lastWordSaved = datetime.datetime.now()
+                #     # wordlist[word] = len(word)
+                #     wordlist[word] = 0
+                # else:
+                #     print("Logging occurence: ", word)
+                #     wordlist[word] = wordlist[word]+1
 
-        print("Saving wordlist: ", str(wordlist))
-        with open('wordlist2.pickle', 'wb') as handle:
+        print("Saving to ",filename, ": ", str(wordlist))
+        with open(filename, 'wb') as handle:
             pickle.dump(wordlist, handle, protocol=pickle.HIGHEST_PROTOCOL)
         time.sleep(12)
