c0b66e55690e88fdc0ed378b934fae6fe8acc0fd
Tony Zhu <snickerton@users.noreply.github.com>
2019-12-03 19:29:01 -0600

    Cleaned up file sys, added final wordlist readable and such

--- mousepos.py
+++ mousepos.py
@@ -3,7 +3,20 @@ import pyperclip
 import time
 import pickle
 
+def exportReadable():
+    d = {}
+    with open('wordlistFinal.pickle', 'rb') as handle:
+        d = pickle.load(handle)
+    readable = open("wordlistReadable.txt", "w")
 
+
+    for x in d.keys():
+        entry = str(x) +","+ str(d[x])
+        readable.write(entry+'\n')
+
+    readable.close()
+
+exportReadable()
 # text = open("mainscreen.txt").read().split()
 # print(text)
 # paste = pyperclip.paste().split()
@@ -12,11 +25,11 @@ import pickle
 # while(True):
 #     print(pyautogui.position())
 #     time.sleep(.5)
-d = {}
-with open('wordlist.pickle', 'rb') as handle:
-    d = pickle.load(handle)
-for x in d.keys():
-    d[x] = 0
-print(d)
-with open('wordlistFinal.pickle', 'wb') as handle:
-    pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# d = {}
+# with open('wordlist.pickle', 'rb') as handle:
+#     d = pickle.load(handle)
+# for x in d.keys():
+#     d[x] = 0
+# print(d)
+# with open('wordlistFinal.pickle', 'wb') as handle:
+#     pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
